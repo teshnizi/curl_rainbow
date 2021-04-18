b1863bd8490258c5247d57c265260e62be1fe018
hkimia <kimia@cs.toronto.edu>
2021-04-18 01:18:00 -0400

    add scripts

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', 
 # TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
 parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
-parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
+parser.add_argument('--enable-cudnn', action='store_false', help='Enable cuDNN (faster but nondeterministic)')
 parser.add_argument('--checkpoint-interval', default=0, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
 parser.add_argument('--memory', help='Path to save/load the memory from')
 parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t zip the memory file. Not recommended (zipping is a bit slower and much, much smaller)')
--- run_curl.sh
+++ run_curl.sh
@@ -1,1 +1,31 @@
-python3 main.py --game ms_pacman
+#!/bin/bash
+
+##############################
+
+#SBATCH --partition=gpu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=4:00:00
+#SBATCH --mem=8GB
+#SBATCH --job-name=rl
+#SBATCH --output=./slurm_out/%j.out
+
+##############################
+
+# ['skiing', 'riverraid', 'asterix', 'zaxxon', 'adventure', 
+# 'tutankham', 'seaquest', 'breakout', 'demon_attack', 'space_invaders', 
+# 'frostbite', 'ice_hockey', 'private_eye', 'carnival', 'phoenix', 
+# 'jamesbond', 'robotank', 'tennis', 'atlantis', 'krull', 'kangaroo', 
+# 'yars_revenge', 'battle_zone', 'pong', 'montezuma_revenge', 'bank_heist', 
+# 'venture', 'beam_rider', 'elevator_action', 'pitfall', 'road_runner', 
+# 'gravitar', 'video_pinball', 'journey_escape', 'bowling', 'boxing',
+# 'double_dunk', 'chopper_command', 'air_raid', 'hero', 'berzerk', 
+# 'name_this_game', 'pooyan', 'kaboom', 'kung_fu_master', 'up_n_down', 
+# 'amidar', 'alien', 'time_pilot', 'solaris', 'freeway', 'wizard_of_wor', 
+# 'star_gunner', 'gopher', 'defender', 'assault', 'qbert', 'ms_pacman', 
+# 'enduro', 'crazy_climber', 'centipede', 'asteroids', 'fishing_derby']
+
+
+python3 main.py --game phoenix --evaluation-interval 128
